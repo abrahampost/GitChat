e03dede116240b149cd29ad0ccfb59cbbeaf73ae
Richard Le <richardle798@gmail.com>
2019-04-24 19:23:26 -0400

    implemented initial parser

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import argparse
 
-parser = make_parser()
-parser.parse_args()
-
 def make_parser():
     parser = argparse.ArgumentParser(description='Chat with your friends using commit messages')
-    parser.add_argument('display', help='display all recent commit messages')
-    parser.add_argument('message', help='send a message to the repository')
+    sp = parser.add_subparsers()
+    display=sp.add_parser('display', help='display all recent commit messages', parents=[parser])
+    message=sp.add_parser('message', help='send a message to the repository', parents=[parser])
+    message.add_argument('--display', action='store_true', default=False, help='display all recent commit messages')
+    #parser.add_argument('--message', nargs=argparse.REMAINDER, default=[], help='send a message to the repository')
     return parser
+
+parser = make_parser()
+args = parser.parse_args()
+
+print(parser)
+
+if args.message:
+    message = " ".join(args.message)
+    print(message)
+elif args.display:
+    # display something here
+    print("I recognized a display")
+else:
+    print("unrecognize command")
